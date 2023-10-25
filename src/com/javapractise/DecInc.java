919f66f0291d162f4790c07d90a6db8aaac76601
Thanusuyaa <tanuselvakumar197@gmail.com>
2023-10-25 13:26:16 +0530

    operators

---
+++ src/com/javapractise/DecInc.java
@@ -0,0 +1,20 @@
+package com.javapractise;
+     // Demonstrate --
+public class DecInc {
+     public static void main(String[] args) {
+     int a=5;
+     int b=3;
+     int c;
+     int d;
+    			
+     c=--b;
+     d=a--;
+     c--;
+    System.out.println("a = "+ a);
+  	System.out.println("b = "+ b);
+    System.out.println("c = "+ c);
+    System.out.println("d = "+ d);	
+
+	}
+
+}
